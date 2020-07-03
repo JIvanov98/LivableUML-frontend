7d8c163c199c0141637620672ad01aa5f09895cd
Joulien <joulien_i@yahoo.com>
2020-07-03 13:07:23 -0700

    using react-draggable library

--- livable-uml/src/pages/editor/element/Element.tsx
+++ livable-uml/src/pages/editor/element/Element.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import Draggable from 'react-draggable';
 
 type Props = {
   x: number,
@@ -14,91 +15,32 @@ const useStyles = makeStyles((theme: Theme) =>
       position: 'absolute',
       flexWrap: 'wrap',
       flexDirection: 'column',
+      padding: '0.5em 1em',
       background: theme.palette.secondary.dark,
-    }
+      border: '3px solid rgba(0, 0, 0, 1)',
+      cursor: 'grab',
+      '&.hover': {
+        background: theme.palette.secondary.light,
+        border: '20px solid rgba(0, 0, 0, 1)',
+      },
+      '&.active': {
+        background: theme.palette.secondary.main,
+      },
+
+    },
   })
 );
 
-// adapted from https://www.kirupa.com/html5/drag.htm
-
 export default function Element(props: Props) {
-  const [active, setActive] = React.useState(false);
-  const [currentCoords, setCurrentCoords] = React.useState({x: props.x, y: props.y});
-  const [initialCoords, setInitialCoords] = React.useState({x: 0, y: 0});
-  const [offsetCoords, setOffsetCoords] = React.useState({x: 0, y: 0});
   const classes = useStyles();
 
-
-  // the event is actually of type React.TouchEvent | React.MouseEvent 
-  // but specifying both results in error (?)
-  let dragStart = (e: any) => {
-    e.type === "touchstart" ? 
-        setInitialCoords({
-          x: e.touches[0].clientX - offsetCoords.x,
-          y: e.touches[0].clientY - offsetCoords.y
-        })
-      :
-        setInitialCoords({
-          x: e.clientX - offsetCoords.x,
-          y: e.clientY - offsetCoords.y
-        });
-    
-    setActive(true);
-  }
-
-  let dragEnd = (e: any) => {
-    setInitialCoords(
-    {
-      x: currentCoords.x,
-      y: currentCoords.y
-    });
-
-
-    setActive(false);
-  }
-
-  let drag = (e: any) => {
-    if (active) {
-    
-      e.preventDefault();
-
-      e.type === "touchmove" ?
-          setCurrentCoords({
-            x: e.touches[0].clientX -  initialCoords.x,
-            y: e.touches[0].clientY -  initialCoords.y
-          })
-        :
-          setCurrentCoords({
-            x: e.clientX -  initialCoords.x,
-            y: e.clientY -  initialCoords.y
-          })
-
-      setOffsetCoords(
-      {
-        x: currentCoords.x,
-        y: currentCoords.y
-      });
-    }
-  }
-
-
   return (
-    <Box
-    draggable
-    className={classes.root}
-    onMouseDown = {dragStart}
-    onTouchStart = {dragStart}
-    onTouchEnd = {dragEnd}
-    onMouseUp = {dragEnd}
-    onTouchMove = {drag}
-    onMouseMove = {drag}
-
-    p={2}
-
-    left={currentCoords.x}
-    top={currentCoords.y}
-    >
-      draggable element test
-    </Box>
+    <Draggable
+      defaultPosition={{x: props.x, y: props.y}}
+      grid={[25, 25]}>
+      <Box className={classes.root}>
+        <p>Drag from here</p>
+      </Box>
+    </Draggable>
   );
 }
