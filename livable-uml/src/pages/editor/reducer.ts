63d652142816ad22fcb19265bffa35308d1cd296
Joulien Ivanov <joulien_i@yahoo.com>
2020-07-07 13:02:15 -0700

    removed console log on reducer.ts

--- livable-uml/src/pages/editor/reducer.ts
+++ livable-uml/src/pages/editor/reducer.ts
@@ -9,7 +9,6 @@ export function editorReducer(
   state = initialState,
   action: UserActionTypes
 ): EditorData {
-  console.log(state.elements);
   switch (action.type) {
     case ADD_ELEMENT:
       return {elements: [...state.elements, action.payload]};
