6fad353913b545f6d3f0c5e24f00cacdb74d1a7b
Joulien Ivanov <joulien_i@yahoo.com>
2020-07-08 12:53:47 -0700

    changed var to let

--- livable-uml/src/pages/editor/toolbar/CreateIconAction.ts
+++ livable-uml/src/pages/editor/toolbar/CreateIconAction.ts
@@ -1,7 +1,7 @@
 import { addElement } from '../actions';
 import { ElementData } from '../types';
 
-var counter = 0;
+let counter = 0;
 
 export default function createIconAction() {
     addElement({id: counter, x: 0, y: 0} as ElementData);
